d154a6ff3dd9e404a76af17ec4e70d4c9b0d301e
azheng00 <azheng00@stuy.edu>
2020-03-17 13:56:30 -0400

    fininshed with curves

---
+++ newscript.py
@@ -0,0 +1,36 @@
+clear
+ident
+#halo
+torus
+250 475 -75 5 50
+rotate
+x 20
+apply
+ident
+#head
+sphere
+250 400 0 50
+#body
+box
+175 340 -25 150 175 50
+#legs
+box
+175 150 -25 70 125 50
+box
+255 150 -25 70 125 50
+arms
+sphere
+350 325 0 25
+sphere
+375 275 0 25
+sphere
+400 225 0 25
+sphere
+150 325 0 25
+sphere
+125 275 0 25
+sphere
+100 225 0 25
+apply
+save
+pic.png
